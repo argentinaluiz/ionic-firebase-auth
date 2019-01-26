454f3d2b540a3a9e3c0ae6bdfd64da3c8b5178e4
Luiz <argentinaluiz@gmail.com>
2019-01-26 10:50:29 -0200

    First commit

---
+++ src/app/firebase-config.ts
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyAiZ8X2TFwPoBks8050yOXZ_bp47QjJgSw",
+    authDomain: "test-ionic-auth-a98bd.firebaseapp.com",
+    databaseURL: "https://test-ionic-auth-a98bd.firebaseio.com",
+    projectId: "test-ionic-auth-a98bd",
+    storageBucket: "test-ionic-auth-a98bd.appspot.com",
+    messagingSenderId: "886480162471"
+};
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,68 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController, Platform} from 'ionic-angular';
+import firebase from 'firebase/app';
+import firebaseConfig from '../../app/firebase-config';
+import 'firebase/auth';
+import 'firebase/database';
+declare const cordova;
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    verificationCode = '';
+    verificationId = '';
+    constructor(public navCtrl: NavController, private platform: Platform) {
+        firebase.initializeApp(firebaseConfig);
 
-  }
+    }
+
+    ionViewDidLoad() {
+        this.platform.ready().then(() => {
+            cordova.plugins.firebase.auth.verifyPhoneNumber('+55XXXXXXXXXXXXXXXXXXXXXXXX', 0)
+                .then(
+                    verificationId => {
+                        this.verificationId = verificationId;
+                        console.log('verificationId',verificationId)
+                    },
+                    error => {
+                        console.log('verificationIdError',error);
+                    }
+                );
+        });
+    }
+
+    verificar() {
+        cordova.plugins.firebase.auth.onAuthStateChanged((userInfo) =>{
+            if (userInfo && userInfo.uid) {
+                console.log('userInfo from Cordova',userInfo);
+                const fbCredential = firebase.auth.PhoneAuthProvider.credential(this.verificationId, this.verificationCode)
+                firebase.auth()
+                    .signInAndRetrieveDataWithCredential(fbCredential)
+                    .then(
+                        (user) => {
+                            console.log('User authenticated',user);
+                        },
+                        (error) => {
+                            console.log('User not authenticated',error);
+                        }
+                    );
+            } else {
+                // user was signed out
+            }
+        });
+        // cordova.plugins.firebase.auth
+        //     .signInWithVerificationId(this.verificationId, this.verificationCode)
+        //     .then(
+        //         (userInfo) => {
+        //             console.log(userInfo)
+        //         },
+        //         (error) => {
+        //             console.log(error);
+        //         }
+        //     )
+    }
 
 }
